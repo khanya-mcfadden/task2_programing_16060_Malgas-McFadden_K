a926a0be50cc6b2115508e919bd90b736d5ac556
khanya-mcfadden <23malgasmk037@collyers.ac.uk>
2025-03-25 12:40:30 +0000

    Add booking route and update navigation in index.html

--- flaskr/main.py
+++ flaskr/main.py
@@ -6,7 +6,9 @@ app = Flask(__name__, template_folder='Templates')
 def hello_world():
     return render_template('index.html')
 
-
+@app.route("/booking")
+def booking():
+    return render_template('booking.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
