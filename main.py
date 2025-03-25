a39a495a35026f55a136f9635c6bbb0479080630
khanya-mcfadden <23malgasmk037@collyers.ac.uk>
2025-03-25 10:37:49 +0000

    Remove obsolete files and add initial Flask application structure

---
+++ main.py
@@ -0,0 +1,14 @@
+import flask  as Flask
+from flask import render_template
+    
+app = Flask.Flask(__name__)
+
+app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
