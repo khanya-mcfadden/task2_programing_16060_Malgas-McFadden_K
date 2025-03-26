73779d27d7ea78d4aeb49c17accf878fe6ac7bca
khanya-mcfadden <23malgasmk037@collyers.ac.uk>
2025-03-26 14:06:59 +0000

    added routes for other pages

--- flaskr/main.py
+++ flaskr/main.py
@@ -5,11 +5,37 @@ app = Flask(__name__, template_folder='Templates')
 @app.route("/")
 def hello_world():
     return render_template('index.html')
+@app.route("/about")
+def about():
+    return render_template('about.html')
 
-@app.route("/booking")
+@app.route("/services")
+def services():
+    return render_template('services.html')
+
+
+
+
+@app.route("booking-choice")
+def booking_choice():
+    return render_template('booking-choice.html')
+
+@app.route("/booking-installation")
+def booking_installation():
+    return render_template('booking-installation.html')
+
+
+@app.route("/booking-consultation")
 def booking():
-    return render_template('booking.html')
+    return render_template('booking-consultation.html')
 
+@app.route("/register")
+def register():
+    return render_template('register.html')
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
 if __name__ == '__main__':
     app.run(debug=True)
     
