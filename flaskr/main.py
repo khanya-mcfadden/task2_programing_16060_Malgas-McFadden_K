b822e6085ab929fc684ee27c0d849d4917f703c2
khanya-mcfadden <23malgasmk037@collyers.ac.uk>
2025-03-27 10:12:54 +0000

    finished login and registraion css and started backend for the login and registraion systems

--- flaskr/main.py
+++ flaskr/main.py
@@ -1,5 +1,20 @@
-from flask import Flask
-from flask import render_template
+from datetime import datetime, timedelta
+import os
+import sqlite3
+from flask import (
+    Flask,
+    jsonify,
+    make_response,
+    redirect,
+    render_template,
+    request,
+    send_from_directory,
+    session,
+    url_for,
+)
+import requests
+from werkzeug.security import generate_password_hash, check_password_hash
+import re
 app = Flask(__name__, template_folder='Templates')
 
 @app.route("/")
@@ -31,11 +46,98 @@ def booking():
 
 @app.route("/register")
 def register():
-    return render_template('register.html')
+        if request.method == 'POST':
+            username = request.form['username']
+            email = request.form['email']
+            password = request.form['password']
+            password_confirm = request.form['password-confirm']
+            # have a input validation
+            if not username or not email or not password or not password_confirm:
+                return "All fields must be filled", 400
+            
+            
+            # input length validation
+            if len(username) > 200 or len(password) > 200 or len(email) > 200:
+                return "Input exceeds character limit", 400
+            if len(username) < 4 or len(password) < 10:
+                return "Username and password must be at least 4 characters", 400
+            
+            
+            
+            # password match validation
+            if password != password_confirm:
+                return "Passwords do not match", 400
+            
+            # email validation
+            if (
+                not re.match("^[a-zA-Z0-9@._!;#$%&'()*+,-./:;<=>?@[\]^_`{|}~]+$", username)
+                or not re.match("^[a-zA-Z0-9@._!;#$%&'()*+,-./:;<=>?@[\]^_`{|}~]+$", email)
+                or not re.match(
+                    "^[a-zA-Z0-9@._!;#$%&'()*+,-./:;<=>?@[\]^_`{|}~]+$", password
+                )
+            ):
+                return "Invalid characters in username, email or password", 400
+
+            password_hash = generate_password_hash(password)
+            connection = sqlite3.connect('users.db')
+            cursor = connection.cursor()
+            cursor.execute(
+                "select * from users where username = ? or email = ?", (username, email)
+            )
+            exsiting_user = cursor.fetchone()
+            if exsiting_user:
+                return "Username or email already exists", 400
+            else:
+                cursor.execute(
+                    "insert into users (username, email, password) values (?, ?, ?)",
+                    (username, email, password_hash),
+                )
+        
+    
+            return render_template('register.html')
+
+
+
 
 @app.route("/login")
 def login():
-    return render_template('login.html')
+    
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        if len(username) > 200 or len(password) > 200 or len(email) > 200:
+            return "Input exceeds character limit", 400
+        
+        connection = sqlite3.connect('users.db')
+        cursor = connection.cursor()
+        cursor.execute(
+            "SELECT username, password, email, admin FROM users WHERE username = ?",
+            (username,),
+        )
+        user = cursor.fetchone()
+        connection.close()
+        if check_password_hash(user[1], password):
+            session["username"] = username
+            session["admin"] = False
+            return redirect(url_for("profile"))
+        else:
+            return "Invalid username or password", 400
+
+    return render_template("login.html")
+
+
+
+
+
+
+
+
+
+
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
     
